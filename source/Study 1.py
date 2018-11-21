48cdf4cdb2e96eae928cc2ce3de7b57a021e2859
KDW <aron0725@naver.com>
2018-11-21 10:21:04 +0900

    1st homework

---
+++ source/Study 1.py
@@ -0,0 +1,17 @@
+def A():
+    price = input('얼마를 가지고 계세요? ')
+    print('입력하신 값은 '+price+'입니다.')
+
+
+def B():
+    print('가나다라마\n바사아자차카타파하')
+
+
+def C():
+    age = int(input('나이를 입력하세요 : '))
+    name = input("이름을 입력하세요 : ")
+    print("나의 이름은 " + name + "이고,", age, "세 입니다.")
+
+
+if __name__ == '__main__':
+    A()
