97cf36b6f6bf7191e0408b80c775bd63bce1c48f
KDW <aron0725@naver.com>
2018-11-26 12:36:47 +0900

    11.26 commit

--- source/Study 2.py
+++ source/Study 2.py
@@ -1,6 +1,15 @@
 def coffee():
     price = int(input("투입할 금액을 입력하세요 : "))
     amount = int(input("몇잔? : "))
+    change = price-amount*200
+    if(change<0):
+        print("돈을 더 넣어주세요!")
+    else:
+        print("거스름돈 : ",change,"원")
 
+def listmul():
+    list1 = ["하나","둘","셋"]
+    list2 = ["양수","한자리수"]
 
 if __name__ == '__main__':
+    coffee()
