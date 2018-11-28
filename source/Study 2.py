68443d8f6d8a3de1d23bb7143b87bed0ef078803
KDW <aron0725@naver.com>
2018-11-28 12:49:38 +0900

    11-28 change

--- source/Study 2.py
+++ source/Study 2.py
@@ -1,3 +1,4 @@
+#A
 price = int(input("투입할 금액을 입력하세요 : "))
 amount = int(input("몇잔? : "))
 change = price-amount*200
@@ -6,6 +7,7 @@ if(change<0):
 else:
     print("거스름돈 : ",change,"원")
 
+#B
 list1 = ["하나","둘","셋"]
 list2 = ["양수","한자리수"]
 print([(x,y) for x in list1 for y in list2])
